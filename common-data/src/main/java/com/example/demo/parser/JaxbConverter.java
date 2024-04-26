592cd31cd01a68fa78c1ac80de944c97bdc563bd
Nakul Bhuwalka <nakulbhuwalka@gmail.com>
2024-04-26 08:09:44 +0530

    moved

---
+++ common-data/src/main/java/com/example/demo/parser/JaxbConverter.java
@@ -0,0 +1,18 @@
+package com.example.demo.parser;
+
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class JaxbConverter {
+
+    public static java.util.Date parseDateTime( String lexicalXSDDateTime ) {
+
+        Instant instant = DateTimeFormatter.ISO_INSTANT.parse(lexicalXSDDateTime,Instant::from);
+        return Date.from(instant);
+    }
+
+    public static String printDateTime( java.util.Date val ) {
+        return  DateTimeFormatter.ISO_INSTANT.format(val.toInstant());
+    }
+}
--- src/main/java/com/example/demo/LEIRecordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import org.gleif.data.schema.leidata._2016.LEIRecordType;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LEIRecordRepository extends MongoRepository<LEIRecordType, Long>
- {
-
-}
